2375b5a8e9e6d16e9972cd7514adbc878c354535
ngerek <ngere.k@outlook.com>
2022-07-10 14:17:26 +0100

    created firestore folder and setup config

---
+++ src/firestore/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import './firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyAPdZrUvppf21ohQTzWCg71k6sdC9j9Fak',
+	authDomain: 'article-site-8f526.firebaseapp.com',
+	projectId: 'article-site-8f526',
+	storageBucket: 'article-site-8f526.appspot.com',
+	messagingSenderId: '283764452123',
+	appId: '1:283764452123:web:2787fe873fe0f8478dc1be'
+};
+
+// initialize app
+firebase.initializeApp(firebaseConfig);
+
+// initialize services
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
