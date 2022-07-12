5640e10ff619b60052cbe28b703559e69d012402
ngerek <ngere.k@outlook.com>
2022-07-12 12:10:12 +0100

    updated file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,25 @@ import './Navbar.css';
 const Navbar = () => {
 	const { logout } = useLogout();
 	const { user } = useContext(AuthContext);
+
+	const styles = {
+		fontFamily: 'Roboto Slab',
+		fontWeight: 'Black',
+		letterSpacing: '0.11em',
+		color: '#fff',
+		marginRight: '15px'
+	};
+
+	const captilizeDisplayName = (str) => {
+		let results = [];
+
+		for (let word of str.split(' ')) {
+			results.push(word[0].toUpperCase() + word.slice(1).toLowerCase());
+		}
+
+		return results.join(' ');
+	};
+
 	return (
 		<nav className="navbar">
 			<ul>
@@ -16,27 +35,36 @@ const Navbar = () => {
 					<li className="title">
 						<Link to="/">ArticleSite</Link>
 					</li>
-					{user && <p style={{ fontWeight: 'bold' }}>hi, {user.displayName}</p>}
+					{user && <p style={styles}>Hi, {captilizeDisplayName(user.displayName)}</p>}
 				</Fragment>
 
 				<li>
 					<Link to="/">Home</Link>
 				</li>
+				{user && (
+					<li>
+						<Link to="/dashboard">Dashboard</Link>
+					</li>
+				)}
 
-				<li>
-					<Link to="/dashboard">Dashboard</Link>
-				</li>
-				<li>
-					<Link to="/login">Login</Link>
-				</li>
-				<li>
-					<Link to="/signup">Signup</Link>
-				</li>
-				<li>
-					<button className="logout" onClick={logout}>
-						Logout
-					</button>
-				</li>
+				{!user && (
+					<Fragment>
+						<li>
+							<Link to="/login">Login</Link>
+						</li>
+						<li>
+							<Link to="/signup">Signup</Link>
+						</li>
+					</Fragment>
+				)}
+
+				{user && (
+					<li>
+						<button className="logout" onClick={logout}>
+							Logout
+						</button>
+					</li>
+				)}
 			</ul>
 		</nav>
 	);
