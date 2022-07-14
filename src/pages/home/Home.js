33d6b360e2df41507e45679cd6f08006c6700b9c
ngerek <ngere.k@outlook.com>
2022-07-14 20:48:24 +0100

    setup home component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useCollection } from '../../hooks/useCollection';
+import ArticleCard from '../../components/ArticleCard';
 
 // styles
 import './Home.css';
 
 const Home = () => {
-	return <div>Home</div>;
+	const { documents, isLoading, error } = useCollection('articles');
+	return (
+		<div className="home">
+			{isLoading && <p className="loading">Loading...</p>}
+			{error && <p className="error">{error}</p>}
+			{documents && <ArticleCard articles={documents} />}
+		</div>
+	);
 };
 
 export default Home;
