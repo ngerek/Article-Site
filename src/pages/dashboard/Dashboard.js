3969c17d2ffd3aca407f62ad4a18269059960e97
ngerek <ngere.k@outlook.com>
2022-07-14 20:50:46 +0100

    setup dashboard component

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import ArticleForm from './ArticleForm';
+import ArticleList from './ArticleList';
+import { useCollection } from '../../hooks/useCollection';
 
 // styles
 import './Dashboard.css';
 
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+	const { user } = useContext(AuthContext);
+	const { documents, error } = useCollection('articles', [ 'uid', '==', user.uid ]);
+
+	return (
+		<div className="container">
+			<div className="content">
+				{error && <p>{error}</p>}
+				{documents && <ArticleList articles={documents} />}
+			</div>
+			<div className="sidebar">
+				<ArticleForm uid={user.uid} />
+			</div>
+		</div>
+	);
 };
 
 export default Dashboard;
