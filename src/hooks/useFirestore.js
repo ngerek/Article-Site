dcc587e9a47ed096a9ab3bbdfd8a6d2e22d40641
ngerek <ngere.k@outlook.com>
2022-07-15 10:06:23 +0100

    created file

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,55 @@
+import { useReducer } from 'react';
+import { db, timestamp } from '../firestore/config';
+
+const initialState = {
+	isLoading: false,
+	error: null,
+	success: null
+};
+
+const firestoreReducer = (state, action) => {
+	switch (action.type) {
+		case 'IS_LOADING':
+			return { ...state, isLoading: true };
+		case 'ADDED_DOCUMENT': {
+			return { isLoading: false, error: null, success: true };
+		}
+		case 'DELETED_DOCUMENT':
+			return { isLoading: false, error: null, success: null };
+		case 'ERROR':
+			return { isLoading: false, error: action.payload, success: true };
+		default:
+			return state;
+	}
+};
+
+export const useFirestore = (collection) => {
+	const [ state, dispatch ] = useReducer(firestoreReducer, initialState);
+
+	// collection ref
+	const ref = db.collection(collection);
+
+	const addDocument = async (doc) => {
+		try {
+			dispatch({ type: 'IS_LOADING' });
+			await ref.add({ ...doc, createdAt: timestamp });
+
+			dispatch({ type: 'ADDED_DOCUMENT' });
+		} catch (error) {
+			dispatch({ type: 'ERROR', payload: error.message });
+		}
+	};
+
+	const deleteDocument = async (id) => {
+		dispatch({ type: 'IS_LOADING' });
+
+		try {
+			await ref.doc(id).delete();
+			dispatch({ type: 'DELETED_DOCUMENT' });
+		} catch (error) {
+			dispatch({ type: 'ERROR', payload: error.message });
+		}
+	};
+
+	return { addDocument, deleteDocument, state };
+};
