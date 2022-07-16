7736e9fe8e0e0b82ab0178515947a0a5b5179b41
ngerek <ngere.k@outlook.com>
2022-07-16 10:43:37 +0100

    udpated Article component

--- src/pages/article/Article.js
+++ src/pages/article/Article.js
@@ -1,8 +1,34 @@
+import { Fragment } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDocument } from '../../hooks/useDocument';
+import { useCapitalize } from '../../hooks/useCapitalize';
+
 // styles
 import './Article.css';
 
 const Article = () => {
-	return <div>Article</div>;
+	const { id } = useParams();
+	const { document, isLoading, error } = useDocument('articles', id);
+	const { captilizeName } = useCapitalize();
+
+	return (
+		<div className="article">
+			{isLoading && <p className="loading">Loading...</p>}
+			{error && <p className="error">{error}</p>}
+			{document && (
+				<Fragment>
+					<h2>{captilizeName(document.title)}</h2>
+					<div>
+						<span>By </span>
+						<span style={{ color: 'black', fontWeight: 'bold' }}>
+							{captilizeName(document.author)}
+						</span> - <span>{document.createdAt.toDate().toDateString()}</span>
+					</div>
+					<p>{document.body}</p>
+				</Fragment>
+			)}
+		</div>
+	);
 };
 
 export default Article;
