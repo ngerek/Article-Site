6ee9ad713d111e9735a08767b750a66cfca4d58e
ngerek <ngere.k@outlook.com>
2022-07-17 11:24:21 +0100

    import mode state

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,24 +1,28 @@
 import { Link } from 'react-router-dom';
 import { useCapitalize } from '../hooks/useCapitalize';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 // styles
 import './ArticleCard.css';
 
 const ArticleCard = ({ articles }) => {
 	const { captilizeName } = useCapitalize();
+	const { mode } = useContext(AuthContext);
 	return (
 		<div className="article-list">
 			{articles.map((article) => (
-				<div key={article.id} className="card">
+				<div key={article.id} className={`card ${mode}`}>
 					<h3>{captilizeName(article.title)}</h3>
 					<div>
 						<span>By </span>
-						<span style={{ color: 'black', fontWeight: 'bold' }}>
+						<span style={{ color: mode === 'light' ? 'black' : '#fff', fontWeight: 'bold' }}>
 							{captilizeName(article.author)}
-						</span> - <span>{article.createdAt.toDate().toDateString()}</span>
+						</span>{' '}
+						- <span>{article.createdAt.toDate().toDateString()}</span>
 					</div>
 					<div>{article.body.slice(0, 101)}...</div>
-					<Link to={`/articles/${article.id}`} className="btn">
+					<Link to={`/articles/${article.id}`} className={`btn ${mode}`}>
 						Read more
 					</Link>
 				</div>
