034b9cd8bcccaa8edad499f0df4f9fb16bed976b
ngerek <ngere.k@outlook.com>
2022-07-18 09:55:18 +0100

    updated file

--- src/pages/dashboard/ArticleList.js
+++ src/pages/dashboard/ArticleList.js
@@ -1,6 +1,8 @@
 import { useFirestore } from '../../hooks/useFirestore';
 import Trashcan from '../../assets/trashcan.svg';
 import { useCapitalize } from '../../hooks/useCapitalize';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 // styles
 import './Dashboard.css';
@@ -9,21 +11,20 @@ const ArticleList = ({ articles }) => {
 	const { deleteDocument } = useFirestore('articles');
 	const { captilizeName } = useCapitalize();
 
+	const { mode } = useContext(AuthContext);
+
+	if (!articles.length) {
+		return <p className={`error ${mode}`}>You have not posted any articles.</p>;
+	}
+
 	return (
-		<ul className="articles">
-			{articles &&
-				articles.map((article) => (
-					<li key={article.id}>
-						<p className="title">{captilizeName(article.title)}</p>
-						{/* <p className="author">{article.author}</p> */}
-						<img
-							className="delete"
-							src={Trashcan}
-							alt="trashcan"
-							onClick={() => deleteDocument(article.id)}
-						/>
-					</li>
-				))}
+		<ul className={`articles ${mode}`}>
+			{articles.map((article) => (
+				<li key={article.id}>
+					<p className="title">{captilizeName(article.title)}</p>
+					<img className="delete" src={Trashcan} alt="trashcan" onClick={() => deleteDocument(article.id)} />
+				</li>
+			))}
 		</ul>
 	);
 };
