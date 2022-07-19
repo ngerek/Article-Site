01ce7641c95a62534f60ca3a592b4704f00e47a5
ngerek <ngere.k@outlook.com>
2022-07-19 11:08:02 +0100

    imported mode state into component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import { useContext } from 'react';
 import { useCollection } from '../../hooks/useCollection';
 import ArticleCard from '../../components/ArticleCard';
+import { AuthContext } from '../../context/AuthContext';
 
 // styles
 import './Home.css';
 
 const Home = () => {
 	const { documents, isLoading, error } = useCollection('articles');
+	const { mode } = useContext(AuthContext);
 	return (
 		<div className="home">
-			{isLoading && <p className="loading">Loading...</p>}
-			{error && <p className="error">{error}</p>}
+			{isLoading && <p className={`loading ${mode}`}>Loading...</p>}
+			{error && <p className={`error ${mode}`}>{error}</p>}
 			{documents && <ArticleCard articles={documents} />}
 		</div>
 	);
